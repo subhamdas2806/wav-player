2e2f1707b87a13f09afede15b2f99c50dc795d77
Subham <dassubha7756@gmail.com>
2025-09-27 22:31:28 +0530

    FINALLY fixed universal path issue for Assets folder

--- wav_player/wav_player.cpp
+++ wav_player/wav_player.cpp
@@ -1,14 +1,17 @@
 #include <Windows.h>
 #include <mmsystem.h>
 #include <iostream>
+using namespace std;
 
 #pragma comment(lib, "winmm.lib")
 
 int main()
 {
-    std::cout << "Playing music \n";
-    PlaySound(TEXT("BohemianRhapsody.wav"), NULL, SND_FILENAME | SND_SYNC);
-    std::cout << "Music over \n";
+	cout << "Playing music \n";
+	// Using the path that jumps up one folder (out of Debug) 
+	// to find the Assets folder next to the project file.
+	PlaySound(TEXT("..\\Assets\\BohemianRhapsody.wav"), NULL, SND_FILENAME | SND_SYNC);
+	cout << "Music over \n";
 
-    return 0;
+	return 0;
 }
